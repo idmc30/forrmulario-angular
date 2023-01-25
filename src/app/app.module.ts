44fae59666f385453286ade50845d9c8414fc559
idmc <id.morales10n@gmail.com>
2023-01-24 23:14:12 -0500

    carga peresosa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
+
+
+
+@NgModule({
+  declarations: [
+    SidemenuComponent
+  ],
+  exports:[
+    SidemenuComponent
+  ],
+
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
