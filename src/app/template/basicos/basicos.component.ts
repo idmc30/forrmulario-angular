446643e74a56a23800a48ec0cda4e78d4417fbe2
idmc <id.morales10n@gmail.com>
2023-02-01 19:10:58 -0500

    usando el vieChild

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,5 +1,5 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -10,16 +10,30 @@ import { NgForm } from '@angular/forms';
 })
 export class BasicosComponent implements OnInit {
 
+
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  guardar( miFormulario: NgForm){
+  // guardar( miFormulario: NgForm){
+
+
+  nombreValido():boolean {
+
+    return this.miFormulario?.controls['producto']?.invalid &&
+    this.miFormulario?.controls['producto']?.touched
+  }
+
+
+  guardar(){
     // guardar(miformulario: any){
       // console.log(miformulario);
-    console.log(miFormulario);
+    // console.log(miFormulario);
 
+    console.log(this.miFormulario);
 
   }
 }
