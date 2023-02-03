4d13665a7da6779a718fcad2e5cf732c7312a441
idmc <id.morales10n@gmail.com>
2023-02-02 23:15:27 -0500

    formulario dinamico primera parte

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,16 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Persona{
+
+  nombre:string,
+  favoritos: Favorito[]
+
+}
+
+
+interface Favorito{
+  id:number,
+  nombre: string
+}
+
+
+
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent{
 
-  constructor() { }
+     persona:Persona = {
+        nombre : 'Irwin',
+        favoritos: [
+          { id: 1, nombre : 'Spiderman 2'},
+          { id: 2, nombre : 'Call of Dufty'},
+        ]
+     };
 
-  ngOnInit(): void {
-  }
+    eliminar(index:number){
+      console.log(index);
+
+      this.persona.favoritos.splice(index, 1)
+
+    }
+
+   guardar(){
+    console.log('fornmulario posteado');
+
+   }
 
 }
