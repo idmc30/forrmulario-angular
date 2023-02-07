3f43e66bb88e217df90cf96e37bcd7e88f21be69
idmc <id.morales10n@gmail.com>
2023-02-07 11:37:25 -0500

    validaciones de formularios y resetear despues de dar guardar

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup,FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -7,7 +7,7 @@ import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
   styles: [
   ]
 })
-export class BasicosComponent{
+export class BasicosComponent implements OnInit{
 
 // formBuilder -> es ayudar a nosotros a crear formulario complejos
   //  miFormulario : FormGroup = new FormGroup({
@@ -17,15 +17,56 @@ export class BasicosComponent{
   //  });
 
   miFormulario : FormGroup = this.fb.group({
-    // validaciones, validaciones asincronas
-    nombre : ['RTX 4080ti'],
-    precio : 0,
-    existencias : 0,
+    // valor,validaciones, validadores sincronos, validadores asincronas
+    nombre : [,[Validators.required, Validators.minLength(3)]],
+    precio : [, [Validators.required,Validators.min(0)]],
+    existencias : [,[Validators.required,Validators.min(0)]],
   })
 
 
   constructor(private fb:FormBuilder) { }
+  ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'Rtx',
+      precio : 1600,
+      existencias:10
+    })
+  }
+
+  campoEsValido(campo:string){
+    // return this.miFormulario.controls[campo]?.errors && this.miFormulario.controls[campo]?.touched;
+
+    return this.miFormulario.get(campo)?.errors && this.miFormulario.get(campo)?.touched
+  }
+
+  maximoTope(){
+ 
+    const precio = this.miFormulario.get('precio')?.value;
+
+    if (precio ==10) {
+      return true
+    }
+
+    return false;
+    
+  }
 
 
+  guardar(){
+
+
+    if ( this.miFormulario.invalid) {
+      // todos mis campos fueron tocados
+      this.miFormulario.markAllAsTouched();
+      return;
+      
+    }
+
+    console.log(this.miFormulario.value);
+
+    this.miFormulario.reset();
+    
+    
+  }
 
 }
